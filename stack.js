2e43975db30aed8f003129b5a2c8c2cc4d6932fe
Ryan Nissen <ryankn852@gmail.com>
2022-04-11 17:35:31 -0500

    Completed Stack

--- stack.js
+++ stack.js
@@ -19,26 +19,38 @@ class Stack {
   /** push(val): add new value to the top of the stack. Returns undefined. */
 
   push(val) {
+    const newNode = new Node(val);
 
+    newNode.next = this.top;
+    this.top = newNode;
+    this.size += 1;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
+    if (this.size === 0) {
+      throw Error("Stack is empty")
+    }
+    const oldTop = this.top;
 
+    this.top = this.top.next;
+    this.size -= 1;
+
+    return oldTop.val;
   }
 
   /** peek(): return the value of the top node in the stack. */
 
   peek() {
-
+    return this.top.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
